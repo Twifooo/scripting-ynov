4264e1d328c59f513d79149f4710d66a79ca6782
7n4xt <abdul.esughi@gmail.com>
2026-01-28 10:14:57 +0100

    quest 6 added

--- Maleks-quests/chapter-6/quest-argparse-Q6.py
+++ Maleks-quests/chapter-6/quest-argparse-Q6.py
@@ -1,2 +1,19 @@
 import argparse
 
+
+def process_arguments(args):
+    print("Nom:", args.name)
+    print("Age:", args.age)
+    print("Ville:", args.city)
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-n', '--name', type=str, help="Votre nom")
+parser.add_argument('-a', '--age', type=int, help="Votre age")
+parser.add_argument('-c', '--city', type=str, help="Votre ville")
+
+args = parser.parse_args()
+
+process_arguments(args)
+
